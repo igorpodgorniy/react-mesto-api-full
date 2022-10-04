043eeb1423072322ecfb09218cf5d2409e03e0a5
Igor Podgorniy <ipod-92@mail.ru>
2022-10-04 19:11:15 +0300

    Добавить логирование и обработку CORS

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const router = require('./routers');
 const errorsProcessing = require('./middlewares/errors-processing');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const allowedCors = [
+  'https://mesto.prakticum.nomoredomains.icu',
+  'http://mesto.prakticum.nomoredomains.icu',
+  'localhost:3000',
+];
 
 const { PORT = 3000 } = process.env;
 
@@ -14,11 +21,34 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
